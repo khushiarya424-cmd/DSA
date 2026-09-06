dfb9c240048592ace527b61978db3e4722a7b956
khushiarya424-cmd <khushiarya424@gmail.com>
2026-09-06 14:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (38.89%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
         stack<int>st;
+
         vector<int>ans;
 
-        for(int i=0;i<nums1.size();i++){
-                int index=-1;
-                int greater=-1;
-            for(int j=0;j<nums2.size(); j++){
-                if(nums2[j]==nums1[i]){
-                    index=j;
-                    break;
-                }
-            }
-            for(int k=index+1; k<nums2.size(); k++){
+        unordered_map<int, int>mpp;
 
+      
+      for(int i=0;i<nums2.size(); i++){
 
-                if(nums2[k]>nums1[i]){
-                    greater=nums2[k];
-                    break;
-                }
-                while(st.empty()==false && nums2[k]>nums1[i]){
-                    st.pop();
-                }
-            
-              st.push(nums2[k]);
-                
-            }
-            ans.push_back(greater);
+        while(st.empty()==false && st.top()<nums2[i]){
+            mpp[st.top()]=nums2[i];
+            st.pop();
         }
-        return ans;
-  
 
+        st.push(nums2[i]);
 
+      }
+
+      //now we are outside the stack
+
+      while(st.empty()==false){
+        //jab tk stack khali ni hota
+        //basically if hum upaar wali for-loop ke bahar aagye h and still stack mein elements kuch remaining hai
+        // that means that there in no greater elements for them
+        // e.g. [1,3,4,2] now here when you are finding greater element for '4' so your stack must be looking like
+        // stack [ 4, 2] now here 2 is obviously not greater than 4 so we don 't put it in map
+        // we come outside of map and here we are basically mappinng those elements to -1 since 
+        // since.. in the given array there's no element greater to these elements on their right side
+
+        mpp[st.top()]=-1;
+        st.pop();
+      }
+
+      for(int i=0;i<nums1.size(); i++){
+        
+        ans.push_back(mpp[nums1[i]]);
+      }
+      return ans;
         
     }
 };

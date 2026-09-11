1ef21edd23504b4d46193c1239ded7d73fdf1272
khushiarya424-cmd <khushiarya424@gmail.com>
2026-09-12 00:53:30 +0530

    Time: 15 ms (30.53%) | Memory: 71.4 MB (70.14%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+
+        return atmostK_odd(nums, k) - atmostK_odd(nums, k-1);
+        
+    }
+
+    int atmostK_odd(vector<int>& nums, int k){
+        int l=0;
+
+        int r=0;
+
+        int odd_digits=0;
+
+        int count=0;
+
+        while(r<nums.size()){
+
+            if(nums[r]%2!=0){
+
+                // nums[l] odd number hai agar
+                odd_digits++;
+            }
+            if(odd_digits>k){
+                while(odd_digits>k){
+                    if(nums[l]%2!=0){
+                        odd_digits--;
+                    }
+                    l++;
+                }
+            }
+
+            if(odd_digits<=k){
+                count=count+(r-l+1);
+            }
+
+            r++;
+
+
+        }
+
+        return count;
+    }
+};
